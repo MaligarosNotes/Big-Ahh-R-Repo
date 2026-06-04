9069b8b8a3ddd1c577a60e81e4c328717db731db
MaligarosNotes <170369572+MaligarosNotes@users.noreply.github.com>
2026-06-04 18:26:06 -0400

    Update analysis.R

--- Stat331/Stat331 A1/analysis.R
+++ Stat331/Stat331 A1/analysis.R
@@ -1,13 +1,14 @@
 # Assignment analysis helpers
 
 
-# load_assignment_data <- function(path = "data/raw/students.csv") {
-#   if (!file.exists(path)) {
-#     stop("Data file not found: ", path)
-#   }
-#   read.csv(path)
-# }
-# 
+load_assignment_data <- function(path = "data/raw/students.csv") {
+  if (!file.exists(path)) {
+    stop("Data file not found: ", path)
+  }
+  read.csv(path)
+}
+
+A1_variates <- load_assignment_data('~/Documents/GitHub/Big-Ahh-R-Repo/Stat331/Stat331 A1/data/raw/A1_variates (1).txt')
 # add_exam_average <- function(df, exam1 = "exam1", exam2 = "exam2") {
 #   if (!all(c(exam1, exam2) %in% names(df))) {
 #     stop("Required columns missing: ", exam1, ", ", exam2)
@@ -54,3 +55,44 @@ t57_0975 <- qt(0.975, 57)
 low <- summary(slr)$coefficients[2,1] - summary(slr)$coefficients[2,2] * t57_0975
 high <- summary(slr)$coefficients[2,1] + summary(slr)$coefficients[2,2] * t57_0975
 c(low,high)
+#g)
+alpha <- x* summary(slr)$coefficients[2,1] - y
+t.test(alpha, mu=0)
+#h)
+  #i) alpha hat:
+alphahat <- ybar - xbar* summary(slr)$coefficients[2,1] 
+  #ii) beta hat:
+betahat <- summary(slr)$coefficients[2,1]
+  #iii) Standard Error of beta hat:
+SEbetahat <- summary(slr)$coefficients[2,2]
+  #iv) sigmahat:
+sigmahat <- sqrt(sum((residuals(slr))^2)/(length(x)-2))
+  #v) he p-value associated with H_0 : beta = 0
+summary(slr)$coefficients[2,4]
+
+#2. 
+truebeta <- c(-200000, 45, -300, 10000, 150000)
+truesigma <- 12000
+A1_variates <- read_table('~/Documents/GitHub/Big-Ahh-R-Repo/Stat331/Stat331 A1/data/raw/A1_variates (1).txt')
+n <- length(A1_variates)
+X <- as.matrix(cbind(rep(1,n),A1_variates))
+set.seed(21066922)       
+
+?rnorm
+#a)
+simulatedresponse <- (truebeta[1]*(rep(1,n)) + 
+                      truebeta[2]*A1_variates$size + 
+                      truebeta[3]*A1_variates$age + 
+                      truebeta[4]*A1_variates$employees + 
+                      truebeta[5]*A1_variates$col + 
+                      rnorm(n,0,truesigma))
+
+simulatedresponse
+             
+polarxy <- function(theta=NULL,r=NULL){
+  
+  r <- 2
+  r*c(cos(theta),sin(theta))
+}
+
+polarxy(0,60)
