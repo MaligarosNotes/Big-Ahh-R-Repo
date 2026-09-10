e143d9c9b585b754c70063a1315157f31fd3f453
MaligarosNotes <170369572+MaligarosNotes@users.noreply.github.com>
2026-09-10 15:05:25 -0400

    Update analysis.R

--- ITR interval censored/analysis.R
+++ ITR interval censored/analysis.R
@@ -3,4 +3,15 @@ DATA =ISLR::Weekly
 DATA[, "Direction"] <- ifelse(DATA[, "Direction"] == "Up", 1, 0)
 DATA
 
+X = DATA[,1:7]
 X
+
+n <- nrow(DATA)
+n
+
+P = rbinom(n,1,0.7)
+A <- 2 * P - 1
+A
+
+R = DATA[,'Today'] * A
+R
