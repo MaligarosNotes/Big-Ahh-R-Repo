fd8a3d80bd6ed6ce5f667ae053efd52ff5cadb70
MaligarosNotes <170369572+MaligarosNotes@users.noreply.github.com>
2026-09-10 17:26:36 -0400

    Update analysis.R

--- ITR interval censored/analysis.R
+++ ITR interval censored/analysis.R
@@ -15,3 +15,12 @@ A
 
 R = DATA[,'Today'] * A
 R
+
+model = glm(P~DATA[,1]+DATA[,2]+DATA[,3]+DATA[,4]+DATA[,5]+DATA[,6]+DATA[,7]+DATA[,8],family=binomial(link = logit))
+summary(model)
+pi_hat <- function(x,a) {
+  if (a = 1){
+    return predict(model, x)
+  }
+  
+}
